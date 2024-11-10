32fffe0b6e0e4d44d6bc6a8a17e477c90aa8e6c6
NamanDamami <damaminaman07@gmail.com>
2024-11-11 00:32:32 +0530

    Update script.js
    function for making matrix

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function generateMatrixInput() {
+    const order = parseInt(document.getElementById("matrix-order").value);
+    const container = document.getElementById("matrix-input-container");
+    container.innerHTML = "";
 
+    let table = "<table>";
+    for (let i = 0; i < order; i++) {
+        table += "<tr>";
+        for (let j = 0; j < order; j++) {
+            table += `<td><input type="number" id="a${i}${j}" step="any"></td>`;
+        }
+        table += "</tr>";
+    }
+    table += "</table>";
+    container.innerHTML = table;
+    document.getElementById("transformation-steps").innerHTML = "";
+}
